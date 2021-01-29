8ced1e8af90dde49ba2731ffe51c561972135397
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-01-29 06:37:52 +0530

    Updated email and version bump.

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ classifiers = [ 'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
-    'Programming Language :: Python',
+    'Programming Language :: Python3',
     ]
 
 setup(
     name = "PlotDigitizer",
-    version = "0.0.4",
+    version = "0.0.5",
     description = "Digitize plots and extract trajectories.",
     long_description = readme,
     long_description_content_type = 'text/markdown',
@@ -22,7 +22,7 @@ setup(
     package_dir = { 'PlotDigitizer' : '.' },
     install_requires = [ 'numpy' ],
     author = "Dilawar Singh",
-    author_email = "dilawars@ncbs.res.in",
+    author_email = "dilawar.s.rajput@gmail.com",
     url = "http://github.com/dilawar/PlotDigitizer",
     license='GPLv3',
     classifiers=classifiers,
