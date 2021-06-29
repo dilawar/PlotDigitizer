fe90dc489687d648a803bf4bf2174a642bd166e3
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-06-29 23:57:40 +0530

    some more progress.

--- plotdigitizer/grid.py
+++ plotdigitizer/grid.py
@@ -11,10 +11,11 @@ TEMP = tempfile.gettempdir()
 
 
 def _save_fig(img, outfile):
+    print(f'Saved to {outfile}')
     cv.imwrite(outfile, img)
 
 
-def remove_horizontal_grid(img) -> np.ndarray:
+def remove_horizontal_grid_simple(img) -> np.ndarray:
     μ, σ = img.mean(), img.std()
     for i, row in enumerate(img):
         if row.mean() < μ - σ:
@@ -22,23 +23,43 @@ def remove_horizontal_grid(img) -> np.ndarray:
             img[i, :] = img.max()
     return img
 
+def heal(orig):
+    kernel = np.ones((3,3), np.uint8)
+    img = cv.morphologyEx(orig.copy(), cv.MORPH_OPEN, kernel, iterations=2)
+    return img
+
 
 def remove_grid(orig) -> np.ndarray:
     img = orig.copy()
-    img = remove_horizontal_grid(img)
-    return remove_horizontal_grid(img.T).T
+    thres = cv.threshold(img, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+    # Remove horizontal lines
+    horizontal_kernel = cv.getStructuringElement(cv.MORPH_RECT, (40,1))
+    remove_horizontal = cv.morphologyEx(thres, cv.MORPH_OPEN, horizontal_kernel, iterations=2)
+    cnts = cv.findContours(remove_horizontal, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        cv.drawContours(img, [c], -1, 255, 2)
+
+    # Remove vertical lines
+    vertical_kernel = cv.getStructuringElement(cv.MORPH_RECT, (1,40))
+    remove_vertical = cv.morphologyEx(thres, cv.MORPH_OPEN, vertical_kernel, iterations=2)
+    cnts = cv.findContours(remove_vertical, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        cv.drawContours(img, [c], -1, 255, 2)
+    return heal(img)
 
 
-def test_remove_grid(imgfile: Path, debug: bool = False):
+def test_remove_grid(imgfile: Path, debug: bool = True):
     img = cv.imread(str(imgfile), 0)
     if debug:
         _save_fig(img, f"{TEMP}/orig.png")
     withoutgrid = remove_grid(img)
-    assert withoutgrid.mean() > img.mean()
+    # assert withoutgrid.mean() > img.mean()
     if debug:
         _save_fig(withoutgrid, f"{TEMP}/without_grid.png")
 
 
 if __name__ == "__main__":
     sdir = Path(__file__).parent
-    test_remove_grid(sdir / "../figures/graph_with_grid.png")
+    test_remove_grid(sdir / "../figures/graph_with_grid.png", True)
