8a1bea1931fd4235d7eb03dd0fd07e16cadc2156
Dilawar Singh <dilawar@subcom.tech>
2022-11-06 18:33:57 +0530

    feat: fixes to tests on Windows

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,15 +12,14 @@ HERE = Path(__file__).parent.resolve()
 
 
 def _run_cmdline(infile: Path, points, locations):
-    cmd = f"plotdigitizer {infile} "
+    cmd = f"plotdigitizer {str(infile)} "
     pts = " ".join([f"-p {','.join(map(str,pt))}" for pt in points])
     locs = " ".join([f"-l {','.join(map(str,pt))}" for pt in locations])
     cmd += f"{pts} {locs}"
     outfile = infile.with_suffix(".result.png")
     trajfile = infile.with_suffix(".result.csv")
-    cmd += f" --plot {outfile} --output {trajfile}"
-    cmd = [x for x in cmd.split(" ") if x.strip()]
-    r = subprocess.run(cmd, check=True)
+    cmd += f" --plot {str(outfile)} --output {trajfile}"
+    r = subprocess.run(cmd, check=True, shell=True)
     assert r.returncode == 0, f"Failed test {r.returncode}"
     return trajfile
 
@@ -34,7 +33,7 @@ def _check_csv_file(csvfile):
 
 def test_trimmeed():
     csvfile = _run_cmdline(
-        HERE / Path("../figures/trimmed.png"),
+        HERE / ".." / "figures" / "trimmed.png",
         [(0, 0), (20, 0), (0, 1)],
         [(22, 26), (142, 23), (23, 106)],
     )
@@ -43,7 +42,7 @@ def test_trimmeed():
 
 def test_graph1():
     csvfile = _run_cmdline(
-        HERE / Path("../figures/graphs_1.png"),
+        HERE / ".." / "figures" / "graphs_1.png",
         [(1, 0), (6, 0), (0, 3)],
         [(165, 52), (599, 51), (85, 151)],
     )
@@ -52,7 +51,7 @@ def test_graph1():
 
 def test_ecg():
     csvfile = _run_cmdline(
-        HERE / Path("../figures/ECGImage.png"),
+        HERE / ".." / "figures" / "ECGImage.png",
         [(1, 0), (5, 0), (0, 1)],
         [(290, 44), (1306, 43), (106, 301)],
     )
@@ -61,7 +60,7 @@ def test_ecg():
 
 def test_grid():
     csvfile = _run_cmdline(
-        HERE / Path("../figures/graph_with_grid.png"),
+        HERE / ".." / "figures" / "graph_with_grid.png",
         [(200, 0), (1000, 0), (0, 50)],
         [(269, 69), (1780, 69), (81, 542)],
     )
